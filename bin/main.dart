d5537c4882325fe6621064cf23d04d59d0dc10cf
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-07-13 23:30:46 -0600

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -22,7 +22,14 @@ final url = Uri.parse('https://reqres.in/api/users?page=2');
 http.get(url).then((res) {
 
     final body = jsonDecode(res.body);
+
     print(body);
+
+    print('page: ${ body['page'] }');
+
+    print("per_page: ${ body['per_page'] }");
+
+    print("id tercer elemento: ${ body['data'][2]['id'] }");
   
   });
 
