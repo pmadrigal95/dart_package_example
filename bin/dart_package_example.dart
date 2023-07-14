ed817189258737f973a477ebc664e15f9a008b9a
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-07-13 22:31:37 -0600

    init

---
+++ bin/dart_package_example.dart
@@ -0,0 +1,5 @@
+import 'package:dart_package_example/dart_package_example.dart' as dart_package_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_package_example.calculate()}!');
+}
---
+++ test/dart_package_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_package_example/dart_package_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
